e3b2492810dd402e8c3d43d55493a57bb47cd580
Israel Macedo da Silva <raelmacedodasilva@gmail.com>
2024-12-05 10:21:40 -0300

    feat: criação de rotas e funções

---
+++ src/controllers/user.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, request
+from src.app import User, db
+from http import HTTPStatus
+from sqlalchemy import inspect
+
+app = Blueprint('user', __name__, url_prefix='/users')
+
+def _create_user():
+    data =  request.json
+    user = User(username=data['username'])
+    db.session.add(user)
+    db.session.commit()
+
+def _list_users():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [
+        {
+            'id': user.id,
+            'username': user.username,
+        } 
+        for user in users
+    ]
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_user():
+    if request.method == 'POST':
+        _create_user()
+        return {'message': 'User created'}, HTTPStatus.CREATED
+    else:
+        return {'message': _list_users()}
+    
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        'id': user.id,
+        'username': user.username
+    }
+    
+
+@app.route('/<int:user_id>', methods=['PATCH'])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    #if 'username' in data:
+    #    user.username = data['username']
+    #    db.session.commit()
+    mapper = inspect(User)
+    for column in mapper.columns:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
+    return {
+        'id': user.id,
+        'username': user.username
+    }
